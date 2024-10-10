6be62b2a1db52b958f49f7a61c507c9f92007d2d
Alfred <alfrednilausen@gmail.com>
2024-10-10 11:36:50 +0200

    Update 40pGame.java

--- 40pGame.java
+++ 40pGame.java
@@ -1,1 +1,17 @@
+class pointGame{
+    public static void main(String args[]) {
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
 
+        var player = new Player[2];
+        System.out.println("Spiller 1 indstast navn");
+        player[0].setName(scanner.nextString());
+        System.out.println("Spiller 2 indstast navn");
+        player[1].setName(scanner.nextString());
+        int count = 0;
+
+        while(player[0].points < 40 || player[1].points < 40) {}
+
+
+    }
+}
